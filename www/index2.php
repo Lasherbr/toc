e2e3a35e93c404b295f3cb46047e81801c9281d7
LasherBR <ricardi@gmail.com>
2021-08-15 11:01:11 -0300

    Inicia navbar colapsada

--- www/index2.php
+++ www/index2.php
@@ -35,7 +35,7 @@ if($_SESSION['logado'] != "S") {
     <div id="wrapper">
 
         <!-- Sidebar -->
-        <ul class="navbar-nav bg-gradient-primary sidebar hide sidebar-dark accordion" id="accordionSidebar" >
+        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion collapse" id="accordionSidebar" >
 
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
