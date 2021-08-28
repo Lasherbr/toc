6b16bfbadf957dbe6264bae4cb877ff8a12a2eca
LasherBR <ricardi@gmail.com>
2021-08-28 13:58:32 -0300

    Layout componentizado

--- www/menu_esquerdo.php
+++ www/menu_esquerdo.php
@@ -1,3 +1,14 @@
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar" >
+  <!-- Sidebar - Brand -->
+  <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+                <div class="sidebar-brand-icon rotate-n-15">
+                    <i class="fas fa-laugh-wink"></i>
+                </div>
+                <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+            </a>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
  <!-- Nav Item - Dashboard -->
  <li class="nav-item active">
                 <a class="nav-link" href="index.php">
@@ -9,3 +20,8 @@
                     <i class="fas fa-fw fa-table"></i>
                     <span>Relatorio</span></a>
             </li>
+
+
+              <!-- Divider -->
+              <hr class="sidebar-divider">
+              </ul>
